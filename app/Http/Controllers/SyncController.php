eebaf69b6d32187acb5eee8de40c364315e33733
Ali Basha <ali.basha.tak@gmail.com>
2022-06-08 21:24:12 +0300

    check if old and new values not null

--- app/Http/Controllers/SyncController.php
+++ app/Http/Controllers/SyncController.php
@@ -53,7 +53,15 @@ class SyncController extends Controller
         }
         return $column;
     }
-
+    /**
+     * basic sync two databaese baesd on audits table     *
+     * @param  string  $local connection name 
+     * @param  string  $remote connection name
+     *
+     *
+     * @throws PublicException
+     * @author Ali Basha
+     */
     private function Sync($local_db_connection, $remote_db_connection)
     {
         //get all unsynced rows from audits table 
@@ -61,58 +69,50 @@ class SyncController extends Controller
         $array_of_operations = (array)$operations;
         //looping through all unsynced rows
         foreach ($array_of_operations as $operate) {
-            //check if the event is insert 
-            if ($operate->event == 'created') {
-                //get new_values (which is column from audits table) as json
-                $new_values_as_json = json_decode($operate->new_values, true);
-                //initialise two arrays to split values and columns to work with raw query
-                $columns = [];
-                $values = [];
+            //get new_values (which is column from audits table) as json
+            $new_values_as_json = json_decode($operate->new_values, true);
+            //get old_values (which is column from audits table) as json
+            $old_values_as_json = json_decode($operate->old_values, true);
+            //initialise two arrays to split values and columns to work with raw query
+            $columns = [];
+            $old_values = [];
+            $new_values = [];
+            if ($new_values_as_json != null) {
                 //loop through new_values
                 foreach ($new_values_as_json as $key => $value) {
                     if ($key != 'id') {
                         array_push($columns, $key);
-                        array_push($values, $value);
+                        array_push($new_values, $value);
                     }
                 }
-                //add created_at field cuz auditable package don't add timestamps to new or old values 
-                array_push($values, $operate->created_at);
-                array_push($columns, 'created_at');
-                //clean values and columns
-                $clean_values = $this->ResolveArrayToFitQuery($values, false);
-                $clean_columns = $this->ResolveArrayToFitQuery($columns, true);
+            }
+            //check if old_values not null
+            if ($old_values_as_json != null) {
+                //loop through old_values
+                foreach ($old_values_as_json as $key => $value) {
+                    if ($key != 'id') {
+                        array_push($old_values, $value);
+                    }
+                }
+            }
+            //add created_at field cuz auditable package don't add timestamps to new or old values 
+            array_push($new_values, $operate->created_at);
+            array_push($old_values, $operate->created_at);
+            array_push($columns, 'created_at');
+            //clean values and columns
+            $clean_new_values = $this->ResolveArrayToFitQuery($new_values, false);
+            $clean_columns = $this->ResolveArrayToFitQuery($columns, true);
+            //check if the event is insert 
+            if ($operate->event == 'created') {
+
                 //collect raw query as string.
-                $insert_new_record_query = 'INSERT INTO `' . $operate->auditable_table . '` ' . $clean_columns . ' VALUES ' . $clean_values;
+                $insert_new_record_query = 'INSERT INTO `' . $operate->auditable_table . '` ' . $clean_columns . ' VALUES ' . $clean_new_values;
                 // run query of inserting new record on remote db
                 DB::connection($remote_db_connection)->select(DB::raw($insert_new_record_query));
                 /** now we must insert the record of local audits table with 'synced'==1  */
                 //reset columns,values
                 $columns = [];
                 $values = [];
-                //parse $oprate (which is the row of audits table that we are working on) as array
-                $operate_as_array =  (array)$operate;
-                //looping throught $operate_as_array 
-                foreach ($operate_as_array as $key => $value) {
-                    //check that key not id and not synced --here we ignore id to avoid conflicts while inserting into db from multiple locations
-                    if ($key != 'id' and $key != 'synced') {
-                        array_push($columns, $key);
-                        array_push($values, $value);
-                    }
-                    //check if key is synced set value to 1 (to store it on remote db as synced) 
-                    if ($key == 'synced') {
-                        array_push($columns, $key);
-                        array_push($values, 1);
-                    }
-                } //end of  operate_as_array foreach
-                //clean values and columns
-                $value = $this->ResolveArrayToFitQuery($values, false);
-                $column = $this->ResolveArrayToFitQuery($columns, true);
-                //collect raw query as string to insert current audits row
-                $insert_current_audits_row_query = 'INSERT INTO `audits` ' . $column . ' VALUES ' . $value;
-                //run query of inserting audits record on remote db
-                DB::connection($remote_db_connection)->select(DB::raw($insert_current_audits_row_query));
-                //run query of inserting audits record on remote db
-                DB::connection($local_db_connection)->select(DB::raw('UPDATE `audits` SET `synced` = 1 WHERE `audits`.`id` =' . $operate->id));
             } //end if(create)
             elseif ($operate->event == 'updated') {
                 //coming soon
@@ -120,6 +120,30 @@ class SyncController extends Controller
             elseif ($operate->event == 'deleted') {
                 //coming soon
             } //end of if delete
+            //parse $oprate (which is the row of audits table that we are working on) as array
+            $operate_as_array =  (array)$operate;
+            //looping throught $operate_as_array 
+            foreach ($operate_as_array as $key => $value) {
+                //check that key not id and not synced --here we ignore id to avoid conflicts while inserting into db from multiple locations
+                if ($key != 'id' and $key != 'synced') {
+                    array_push($columns, $key);
+                    array_push($values, $value);
+                }
+                //check if key is synced set value to 1 (to store it on remote db as synced) 
+                if ($key == 'synced') {
+                    array_push($columns, $key);
+                    array_push($values, 1);
+                }
+            } //end of  operate_as_array foreach
+            //clean values and columns
+            $value = $this->ResolveArrayToFitQuery($values, false);
+            $column = $this->ResolveArrayToFitQuery($columns, true);
+            //collect raw query as string to insert current audits row
+            $insert_current_audits_row_query = 'INSERT INTO `audits` ' . $column . ' VALUES ' . $value;
+            //run query of inserting audits record on remote db
+            DB::connection($remote_db_connection)->select(DB::raw($insert_current_audits_row_query));
+            //run query of inserting audits record on remote db
+            DB::connection($local_db_connection)->select(DB::raw('UPDATE `audits` SET `synced` = 1 WHERE `audits`.`id` =' . $operate->id));
         } //end of main foreach
     } //end of Sync function
 }
